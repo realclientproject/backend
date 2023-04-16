f4b58b7acf67ed9f7515d6a3f88ad192da87d292
fatima-katrib <fatimakatrib19@gmail.com>
2023-04-17 00:38:51 +0300

    add user router

--- routes/user_route.js
+++ routes/user_route.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import UserController from '../controllers/user_controller.js';
+import express from "express";
+import UserController from "../controllers/user_controller.js";
 const router = express.Router();
 
-router.get('/', UserController.getAllUsers);
-router.get('/:id', UserController.getUser);
-router.put('/:id', UserController.editUser);
-router.delete('/:id', UserController.deleteUser);
-router.post('/', UserController.addUser)
-router.post('/login', UserController.login)
-router.post('/logout', UserController.logout)
+router.get("/:id", UserController.getUser);
+router.get("/", UserController.getAllUsers);
+router.put("/:id", UserController.editUser);
+router.delete("/:id", UserController.deleteUser);
+router.post("/", UserController.addUser);
+router.post("/login", UserController.login);
+router.post("/logout", UserController.logout);
 
 export default router;
